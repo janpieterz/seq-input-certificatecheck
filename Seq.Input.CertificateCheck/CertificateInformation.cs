9fa4040b9170bad7a97c45b1bc8cf36c5cf75e33
David Wedrat <david.wedrat@deswik.com>
2022-07-22 17:27:51 +1000

    Added Issuer, Subject, Thumbprint, SerialNumber, and SubjectAlternativeNames properties to CertificateCheckResult

---
+++ Seq.Input.CertificateCheck/CertificateInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Seq.Input.CertificateCheck
+{
+    public class CertificateInformation
+    {
+        public string Subject { get; set; }
+        public string Issuer { get; set; }
+        public DateTime? LastExpiration { get; set; }
+        public string Thumbprint { get; set; }
+        public string SerialNumber { get; set; }
+        public IEnumerable<string> SubjectAlternativeNames { get; set; }
+    }
+}
---
+++ Seq.Input.CertificateCheck/Extensions/Certificate2Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace Seq.Input.CertificateCheck.Extensions
+{
+    public static class Certificate2Extensions
+    {
+        public static IEnumerable<string> ParseSubjectAlternativeNames(this X509Certificate2 cert)
+        {
+            Regex sanRex = new Regex(@"^DNS Name=(.*)", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+            var sanList = from X509Extension ext in cert.Extensions
+                where ext.Oid.FriendlyName.Equals("Subject Alternative Name", StringComparison.Ordinal)
+                let data = new AsnEncodedData(ext.Oid, ext.RawData)
+                let text = data.Format(true)
+                from line in text.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                let match = sanRex.Match(line)
+                where match.Success && match.Groups.Count > 0 && !string.IsNullOrEmpty(match.Groups[1].Value)
+                select match.Groups[1].Value;
+
+            return sanList;
+        }
+    }
+}
